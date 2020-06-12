7dcb4ce3b565758a62770881d482a4e97711abbc
bzoracler <50305397+bzoracler@users.noreply.github.com>
2020-06-12 23:26:20 +1200

    Update cnn-predict-stage2.py

--- scripts/cnn-predict-stage2.py
+++ scripts/cnn-predict-stage2.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-# Badri Adhikari, 5-21-2017
 # Make the prediction at stage2 with second-stage feature files
 
 import sys
@@ -11,16 +9,16 @@ dir_config = sys.argv[1]
 fileX      = sys.argv[2]
 fileRR     = sys.argv[3]
  
-print ''
+print('')
 print('SCRIPT     : ' + sys.argv[0])
 print('dir_config : ' + dir_config )
 print('fileX      : ' + fileX      )
 print('fileRR     : ' + fileRR  )
-print ''
+print('')
  
 weight_arch_dict = {}
 for i in range(1, 21):
-	print 'Reading weight file ' + dir_config + '/stage2-' + str(i) + '.hdf5 ..'
+	print('Reading weight file ' + dir_config + '/stage2-' + str(i) + '.hdf5 ..')
 	weight_arch_dict[dir_config + '/stage2-' + str(i) + '.hdf5'] = dir_config + '/model-arch.config'
 
 # Need to make X slightly bigger than L x L by padding zeros
@@ -39,12 +37,12 @@ x = getX(fileX, LMAX)
 F = len(x[0, 0, :])
 X = np.zeros((1, LMAX, LMAX, F))
 X[0, :, :, :] = x
-print ''
+print('')
 print_feature_summary(X)
 
 # Predict at (L+10) x (L+10) and trim it back to L x L
-print ''
-print 'Starting ensemble prediction.. '
+print('')
+print('Starting ensemble prediction.. ')
 P = make_ensemble_prediction(weight_arch_dict, X)
 PF = ((P[0].reshape(LMAX, LMAX))[0:L, 0:L]).flatten()
 
